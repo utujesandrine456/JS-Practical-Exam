fe658711136d8b0fcd50cbdf531788ddc443ff2f
utujesandrine456 <utujesandrine456@gmail.com>
2025-06-16 16:32:50 +0200

    Express App

--- Express.js
+++ Express.js
@@ -1,40 +1,53 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-
+const express = require('express');
+const app = express();
+const port = 3000;
 
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: false} ));
 
 
-const students = [];
+let students = [];
+
+class Student {
+    constructor(name, grade) {
+        this.name = name;
+        this.grade = grade;
+    }
+
+    getDetails() {
+        return `Name: ${this.name}, Grade: ${this.grade}`;
+    }
+}
 
 
 app.post('/students', (req, res) => {
-    const newuser = req.body;
+    try {
+        const { name, grade } = req.body;
+        
+        const newStudent = new Student(name, grade);
+        students.push(newStudent);
 
-    if(newuser){
-        newuser.id = req.body.id;
-        newuser.name = req.body.name;
-        newuser.grade = req.body.grade;
+        res.status(201).json({ 
+            message: 'Student added successfully', 
+            student: newStudent.getDetails() 
+        });
 
-        students.push(newuser);
-
-        res.status(200).json({ statusCode: 200, message: "User Added successfully!!!" });
-    } else{
-        res.status(404).json({statusCode: 404, message: "Please check your body" });
+    } catch (error) {
+        res.status(500).json({ error: 'Error adding student' });
     }
-})
-
-
-app.get('/students', (req, res) => {
-
-    res.status(201).json(students);
-
 });
 
 
+app.get('/students', (req, res) => {
+    try {
+        const studentDetails = students.map(student => student.getDetails());
+        res.json(studentDetails);
+        
+    } catch (error) {
+        res.status(500).json({ error: 'Error retrieving students' });
+    }
+});
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
